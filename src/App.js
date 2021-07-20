f6c78dddb2c22a042dd25e3f1e4cbd2d43902d13
John Bae <john21225@gmail.com>
2021-07-20 11:10:03 -0700

    Initialized application and added folder structure

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+function App() {
+	return (
+		<React.Fragment>
+			<Header />
+			<Footer />
+		</React.Fragment>
+	);
+}
+
+export default App;
