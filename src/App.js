cca8d20bd301637b3de900806df933e5e0f5fd59
Kevin Mathis <kevinmathis08@gmail.com>
2021-07-28 22:29:21 -0400

    React state hook refactor

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import Home from "./pages";
+import NotFound from "./NotFound";
 
 function App() {
 	return (
 		<Router>
 			<Switch>
 				<Route path='/' component={Home} exact />
+				<Redirect to='/forbidden' path='' component={NotFound} />
 			</Switch>
 		</Router>
 	);
--- src/components/InputsSection/index.js
+++ src/components/InputsSection/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
 	InputSectionContainer,
 	BannerMessage,
@@ -16,40 +16,25 @@ import {
 	FormOutput,
 } from "./InputsSectionElements";
 
+
+
 const InputSection = () => {
+	const [gender, setGender] = useState(null);
+	const [activity, setActivity] = useState(null);
+	const [age, setAge] = useState(null);
+	const [height, setHeight] = useState(null);
+	const [weight, setWeight] = useState(null);
+	const [calories, setCalories] = useState(null);
+
 	function calculateCalories() {
-		// Checks and stores gender value
-		let gender = null;
-		document.getElementsByName("gender").forEach((radio) => {
-			if (radio.checked) {
-				gender = radio.value;
-			}
-		});
+		let calcHeight = height.feet + height.inch / 12;
+		
 		console.log("gender: " + gender);
-
-		// Checks and stores activity level
-		let activity = null;
-		document.getElementsByName("activity").forEach((radio) => {
-			if (radio.checked) {
-				activity = radio.value;
-			}
-		});
 		console.log("activity: " + activity);
-
-		// Checks and stores age value
-		let age = Number(document.getElementsByName("age")[0].value);
 		console.log("age: " + age);
-
-		// Checks and stores height value
-		let height =
-			Number(document.getElementsByName("height-feet")[0].value) +
-			Number(document.getElementsByName("height-inch")[0].value / 12);
 		console.log("height: " + height);
-
-		// Checks and stores weight value
-		let weight = Number(document.getElementsByName("weight")[0].value);
 		console.log("weight: " + weight);
-
+	
 		// Sets the activity multiplier
 		let activityMult = 0;
 		if (activity === "low") {
@@ -63,25 +48,25 @@ const InputSection = () => {
 		} else if (activity === "extreme") {
 			activityMult = 2.1;
 		}
-
+	
 		// Does calculations based off of BMR
 		let calories = 0;
 		if (gender === "male") {
-			calories = 66 + 6.2 * weight + 12.7 * height - 6.76 * age;
+			calories = 66 + 6.2 * weight + 12.7 * calcHeight - 6.76 * age;
 			calories *= activityMult;
 		} else if (gender === "female") {
-			calories = 655.1 + 4.35 * weight + 4.7 * height - 4.7 * age;
+			calories = 655.1 + 4.35 * weight + 4.7 * calcHeight - 4.7 * age;
 			calories *= activityMult;
 		}
-
+	
 		// Calculates caloric deficit to lose 1% of weight per week
 		let onePercent = (weight * 0.01 * 3500) / 7;
 		console.log(onePercent);
-
-		// Returns the amount of calories
-		document.getElementById("calories").innerHTML =
-			"Eat " + Math.trunc(calories) + " Calories per Day 🔥";
+	
+	
+		setCalories(calories)
 	}
+
 
 	return (
 		<InputSectionContainer>
@@ -96,6 +81,7 @@ const InputSection = () => {
 						name='gender'
 						value='male'
 						id='male'
+						onChange={(e) => setGender(e.target.value)}
 					></FormInputRadio>
 					<FormLabel htmlFor='male'>Male</FormLabel>
 					<FormInputRadio
@@ -103,6 +89,7 @@ const InputSection = () => {
 						name='gender'
 						value='female'
 						id='female'
+						onChange={(e) => setGender(e.target.value)}
 					></FormInputRadio>
 					<FormLabel htmlFor='female'>Female</FormLabel>
 				</FormInputs>
@@ -114,41 +101,46 @@ const InputSection = () => {
 						name='activity'
 						value='low'
 						id='low'
+						onChange={(e) => setActivity(e.target.value)}
 					></FormInputRadio>
-					<FormLabel htmlFor='male'>None</FormLabel>
+					<FormLabel htmlFor='low'>None</FormLabel>
 					<FormInputRadio
 						type='radio'
 						name='activity'
 						value='light'
 						id='light'
+						onChange={(e) => setActivity(e.target.value)}
 					></FormInputRadio>
-					<FormLabel htmlFor='female'>1-3 days</FormLabel>
+					<FormLabel htmlFor='light'>1-3 days</FormLabel>
 					<FormInputRadio
 						type='radio'
 						name='activity'
 						value='moderate'
 						id='moderate'
+						onChange={(e) => setActivity(e.target.value)}
 					></FormInputRadio>
-					<FormLabel htmlFor='female'>4-5 days</FormLabel>
+					<FormLabel htmlFor='moderate'>4-5 days</FormLabel>
 					<FormInputRadio
 						type='radio'
 						name='activity'
 						value='active'
 						id='active'
+						onChange={(e) => setActivity(e.target.value)}
 					></FormInputRadio>
-					<FormLabel htmlFor='female'>6-7 days</FormLabel>
+					<FormLabel htmlFor='active'>6-7 days</FormLabel>
 					<FormInputRadio
 						type='radio'
 						name='activity'
 						value='extreme'
 						id='extreme'
+						onChange={(e) => setActivity(e.target.value)}
 					></FormInputRadio>
-					<FormLabel htmlFor='female'>Extreme</FormLabel>
+					<FormLabel htmlFor='extreme'>Extreme</FormLabel>
 				</FormInputs>
 
 				<FormInputTitle>Age</FormInputTitle>
 				<FormInputs>
-					<FormInputAge type='text' name='age' id='age'></FormInputAge>
+					<FormInputAge type='text' name='age' id='age' onChange={(e) => {setAge(parseInt(e.target.value))}}></FormInputAge>
 					<FormLabel htmlFor='age'></FormLabel>
 				</FormInputs>
 
@@ -158,12 +150,14 @@ const InputSection = () => {
 						type='text'
 						name='height-feet'
 						id='height-feet'
+						onChange={(e) => setHeight({...height, feet: e.target.value})}
 					></FormInputHeight>
 					<FormLabel htmlFor='height-feet'>Feet</FormLabel>
 					<FormInputHeight
 						type='text'
 						name='height-inch'
 						id='height-inch'
+						onChange={(e) => setHeight({...height, inch: e.target.value})}
 					></FormInputHeight>
 					<FormLabel htmlFor='height-inch'>Inches</FormLabel>
 				</FormInputsWrapper>
@@ -174,6 +168,7 @@ const InputSection = () => {
 						type='text'
 						name='weight'
 						id='weight'
+						onChange={(e) => {setWeight(parseInt(e.target.value))}}
 					></FormInputWeight>
 					<FormLabel htmlFor='weight'>Lbs</FormLabel>
 				</FormInputsWrapper>
@@ -183,7 +178,9 @@ const InputSection = () => {
 				</FormSubmitButton>
 			</FormInputsContainer>
 			<FormOutputContainer>
-				<FormOutput id='calories'></FormOutput>
+				{calories && <FormOutput id='calories'>
+					Eat {Math.trunc(calories)} Calories per Day 🔥
+				</FormOutput>}
 			</FormOutputContainer>
 		</InputSectionContainer>
 	);
